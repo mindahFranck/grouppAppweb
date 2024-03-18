11b829290263dfc0a621b6bc540dbe5104d517df
Koder2704 <danielkembeu82@gmail.com>
2024-03-19 00:58:41 +0100

    QUARTIER DETAILS OK

--- src/app/administration/quartiers/details-quartiers/details-quartiers.component.ts
+++ src/app/administration/quartiers/details-quartiers/details-quartiers.component.ts
@@ -4,21 +4,22 @@ import { BreadcrumbService } from 'src/app/app.breadcrumb.service';
 import { FormService } from 'src/app/demo/service/base.service';
 
 type Commune = {
-  id: number,
-  code: string,
-  libelle: string,
-  createdAt: Date,
-  updatedAt: Date
+  id: number;
+  code: string;
+  commune: string;
+  departement_id: number;
+  createdAt: Date;
+  updatedAt: Date;
 };
 // Type de données acceptables par un quartier
 type QuartierDetails<T> = {
-  id: number,
-  libelle: string,
-  idCommunes: number,
-  createdAt: Date,
-  updatedAt: Date,
-  commune: T
-  residences: string[]
+  id: number;
+  libelle: string;
+  idCommunes: number;
+  createdAt: Date;
+  updatedAt: Date;
+  commune: T;
+  residences: string[];
 };
 
 
@@ -29,12 +30,12 @@ type QuartierDetails<T> = {
 })
 export class DetailsQuartiersComponent implements OnInit {
 
-  constructor(private activatedRoute: ActivatedRoute, private service: FormService,private breadcrumbService: BreadcrumbService) {
-      this.breadcrumbService.setItems([
-        { label: 'Liste Des Quartiers', routerLink:"/administration/quartiers/list"},
-        { label: 'Details Quarties'},
+  constructor(private activatedRoute: ActivatedRoute, private service: FormService, private breadcrumbService: BreadcrumbService) {
+    this.breadcrumbService.setItems([
+      { label: 'Liste Des Quartiers', routerLink: "/administration/quartiers/list" },
+      { label: 'Details Quarties' },
     ]);
-    
+
   }
 
   // Declaration de la propriete qui contiendra l'identifiant recupere dans l'url.
@@ -43,6 +44,9 @@ export class DetailsQuartiersComponent implements OnInit {
   loader = false;
   hasResidences = false;
 
+  // Getting the department's name:
+  subInfo: any;
+
   ngOnInit() {
     // Recupere l'id dans l'url.
     this.quartierID = this.activatedRoute.snapshot.params['id'];
@@ -50,23 +54,34 @@ export class DetailsQuartiersComponent implements OnInit {
     this.getQuartierDetails();
   }
 
+  // Getting the departement's name
+  get departement() {
+    this.service.getById("departements", this.quartierDetails?.commune.departement_id).subscribe({
+      next: department => this.subInfo = department,
+      error: error => console.error('Observable emitted an error: ' + error)
+    });
+
+    return this.subInfo.departement;
+  }
+
 
   // Fonction pour recuperer les details d'un quartier
   getQuartierDetails() {
     this.service.getById('quartiers', this.quartierID).subscribe({
-      next: data =>{
-          this.quartierDetails = data;
-          console.log(this.quartierDetails);
+      next: data => {
+        this.quartierDetails = data;
+        console.log(this.quartierDetails);
       },
       error: err => console.error('Observable emitted an error: ' + err),
-      complete: () =>      this.loader = false,
-   });
+      complete: () => this.loader = false,
+    });
     console.log(this.quartierDetails);
+
   }
 
   // Fonction qui verifie si un quartier a ou non une residence
   checkHasResidences() {
-    if (this.quartierDetails.residences.length = 0) this.hasResidences = false;
-    else this.hasResidences = true
+    this.quartierDetails.residences.length == 0 ? this.hasResidences = false : this.hasResidences = true;
+    return this.hasResidences;
   }
 }
--- src/app/administration/quartiers/list-quartiers/list-quartiers.component.ts
+++ src/app/administration/quartiers/list-quartiers/list-quartiers.component.ts
@@ -14,7 +14,7 @@ import { FormService } from 'src/app/demo/service/base.service';
 export class ListQuartiersComponent {
   load: boolean = false;
   titlePage = 'Quartiers';
-  formTitle:any;
+  formTitle: any;
   state: boolean = false;
   deleteDialog: boolean = false;
   formState: boolean = false;
@@ -35,16 +35,16 @@ export class ListQuartiersComponent {
     { name: 'libelle', label: 'Libelle', type: 'text', validators: [Validators.required] },
   ];
 
-  
 
-  
-  
-  constructor(private service: FormService, private fb: FormBuilder, private breadcrumbService: BreadcrumbService,private messageService: MessageService,) { 
+
+
+
+  constructor(private service: FormService, private fb: FormBuilder, private breadcrumbService: BreadcrumbService, private messageService: MessageService,) {
     this.breadcrumbService.setItems([
-      { label: 'Liste Des Quartiers'},
-  ]);
+      { label: 'Liste Des Quartiers' },
+    ]);
   }
-  
+
   ngOnInit(): void {
     this.getAlls();
     this.getAllCommunes();
@@ -81,10 +81,10 @@ export class ListQuartiersComponent {
   }
 
   edit(val: any) {
-    this.temporaile= {};
+    this.temporaile = {};
     this.formTitle = "Modifier une Quartier";
-      this.temporaile = { ...val };
-      this.formState = true;
+    this.temporaile = { ...val };
+    this.formState = true;
   }
 
 
@@ -93,7 +93,7 @@ export class ListQuartiersComponent {
 
 
   add() {
-    this.temporaile= {};
+    this.temporaile = {};
 
     this.formTitle = "Ajouter une Quartier";
     this.formState = true;
@@ -124,58 +124,58 @@ export class ListQuartiersComponent {
   }
   onGlobalFilter(table: Table, event: Event) {
     table.filterGlobal((event.target as HTMLInputElement).value, 'contains');
-}
-//open delete Dialog
+  }
+  //open delete Dialog
 
-async opendeleteDialog(val: any) {
-  this.temporaile = { ...val };
-  this.deleteDialog = true;
+  async opendeleteDialog(val: any) {
+    this.temporaile = { ...val };
+    this.deleteDialog = true;
 
-}
+  }
 
-//editer quartier
-modifier(){
-  if (this.form.invalid) {
-    // Marquez tous les champs comme touchés pour afficher les erreurs
-    this.form.markAllAsTouched();
-  }else{
-    this.state = true
-    this.service.update("quartiers", this.form.value, this.form.value.id).subscribe({
-        next: value =>    console.log(value) ,
+  //editer quartier
+  modifier() {
+    if (this.form.invalid) {
+      // Marquez tous les champs comme touchés pour afficher les erreurs
+      this.form.markAllAsTouched();
+    } else {
+      this.state = true
+      this.service.update("quartiers", this.form.value, this.form.value.id).subscribe({
+        next: value => console.log(value),
         error: err => {
-                   this.state = false ,
-                   this.messageService.add({ severity: 'error', summary: 'Erreur', detail: err.message, life: 3000 });
-                },
-        complete: () =>      {
-          this.state = false; 
+          this.state = false,
+            this.messageService.add({ severity: 'error', summary: 'Erreur', detail: err.message, life: 3000 });
+        },
+        complete: () => {
+          this.state = false;
           this.form.reset();
           this.formState = false
 
           this.ngOnInit();
 
-            this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Ajouté avec success', life: 3000 });
+          this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Ajouté avec success', life: 3000 });
         },
 
-    });
+      });
+    }
   }
-}
-//supprimer un quartier
+  //supprimer un quartier
 
-deleteQuartier(){
-  this.state = true;
-  this.service.delete("quartiers"+"/"+this.temporaile.id).subscribe((result) => {
-    this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Supprimer avec success', life: 3000 });
-    this.state = false;
+  deleteQuartier() {
+    this.state = true;
+    this.service.delete("quartiers" + "/" + this.temporaile.id).subscribe((result) => {
+      this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Supprimer avec success', life: 3000 });
+      this.state = false;
 
-    this.deleteDialog = false;
-    this.ngOnInit();
-    this.temporaile = {};
+      this.deleteDialog = false;
+      this.ngOnInit();
+      this.temporaile = {};
 
-  },
-  (error) =>{
-    this.messageService.add({ severity: 'error', summary: 'Erreur', detail: error.message, life: 5000 });
-    this.state = false;
-  })
+    },
+      (error) => {
+        this.messageService.add({ severity: 'error', summary: 'Erreur', detail: error.message, life: 5000 });
+        this.state = false;
+      })
 
-}
+  }
 }
